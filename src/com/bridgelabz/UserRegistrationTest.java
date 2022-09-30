4348f5d9586dec78b1a40653df938796724f267a
Your Name <you@example.com>
2022-09-30 15:36:29 +0530

    added:Welcome

---
+++ src/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    UserRegistration userRegistration = new UserRegistration();
+
+
+    @Test
+    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Poonam");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("poonam");
+        Assertions.assertEquals(false, result);
+    }
+}
+
